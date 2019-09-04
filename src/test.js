dc7eb8cee9de4d7b56750f461b23b1585f2f4a94
Patrick Hutchinson <patrick_hutchinson@highend.com>
2019-09-04 18:16:51 -0500

    Update test.js
    Lots of hard coded regular expressions to rip the strings apart

--- src/test.js
+++ src/test.js
@@ -2,103 +2,124 @@
 const fs = require('fs');
 const pdf = require('pdf-parse');
  
-let dataBuffer = fs.readFileSync('./test/statement.pdf');
+
+parseFile('./test/eStmt_2017-01-27.pdf')
+//parseFile('./test/eStmt_2017-02-24.pdf')
+//parseFile('./test/eStmt_2017-03-29.pdf')
+
+function parseFile(pdfFile) {
+    let dataBuffer = fs.readFileSync(pdfFile);
+
+    let arr = [];
  
-pdf(dataBuffer).then(data => {
-    // number of pages
-    // console.log(data.numpages);
-    // number of rendered pages
-    // console.log(data.numrender);
-    // // PDF info
-    //console.log(data.info);
-    //console.log(data.info.Author);
+    pdf(dataBuffer).then(data => {
 
-    if ( data.info.Author === "CapitalOne" ) {
-        console.log("Capital One Detected");
-    } else if ( data.info.Author === "Bank of America" ) {
-        console.log("Bank of America");
-        //console.log(data.text);
+        if ( data.info.Author === "CapitalOne" ) {
+            console.log("Capital One Detected");
+        } else if ( data.info.Author === "Bank of America" ) {
+            console.log("Bank of America");
+   
+            // split the large string into individual lines
+            const strArr = data.text.split("\n");
 
-        //const pattern = "(\d{2}\/\d{2}\/\d{2})";
+            // loop over each line in the string array
+            for(const x in strArr){
 
-        //const matches = data.text.match(/(\d{2}\/\d{2}\/\d{2})/ig);
-        //const matches = data.text.match(/(.*)([0-9]{1,3},([0-9]{3},)*[0-9]{3}|[0-9]+)(\.[0-9][0-9])?/igm);
+                // date match
+                const mLine = strArr[x].match(/((\d+\/){2}\d+)(.*)/ig)
 
-        //const matches = data.text.match(/((\d+\/){2}\d+).*(\d+\,)?(\d*)(\.\d{2})?/ig);
+                // skip the interest earned lines
+                if ( strArr[x].match(/^(?:\d+\/\d+\/\d+)(Interest\sEarned)(.*)/g) ) {
+                    //console.log("Interest Earned : ", strArr[x]);
+                    continue;
+                }
 
-        //const matches = data.text.match(/^((\d+\/){2}\d+)(?:\w*\s\w*).*$/img);        
-        // console.log(matches);
-        //console.log(matches.length);
-        //console.log(typeof data.text);
-        // const file = data.text.join();
-        // console.log("file: ",file);
+                if ( mLine ) {
 
-
-        const strArr = data.text.split("\n");
-        console.log(strArr.length);
-
-        for(const x in strArr){
-            //console.log(strArr[x])
-
-
-            const mLine = strArr[x].match(/((\d+\/){2}\d+)(.*)(\d+\,)?(\d*)(\.\d{2})?/ig)
-            if ( mLine ) {
-                console.log("Line: ",...mLine);
-                const { date } = mLine;
-
-                /* const mCur = mLine.match(/(\d+\,)?(\d*)(\.\d{2})/ig);
-                if ( !mCur ) {
-                    console.log(mCur);
-                } */
+                    // currency match
+                    const mCur = mLine[0].match(/(\d+\,)?(\d*)(\.\d{2})$/ig);
+                    // if the current line does not currency match the end of the string
+                    if ( !mCur ) {
+                        // if the current line does not end with a currency match then
+                        // I need to look ahead for the rest of the line data hense the
+                        // finishLine method
+                        const line = finishLine(strArr, x);
+                        arr.push(line);
+                        //console.log(line);
+                    } else {
+                        arr.push(mLine[0]);
+                    }
+                }
             }
+
+            return arr;
+
+        } else if ( data.info.Author === "AmEx" ) {
+            console.log("AmEx");
         }
-        
+    }).then( arrayOfItems => {
+        // arrayOfItems.forEach( a => console.log(a));
+        console.log("Length: ", arrayOfItems.length);
 
+        for(const item of arrayOfItems) {
+            console.log( parseBofA(item) );
+        }
 
+    }).catch( error => {
+        console.log("Error: ", error);
+    });
 
+}
 
+// this needs to be a recursive function but
+// for now this will work
+function finishLine(strArr, currentLine){
+    return strArr[currentLine] + strArr[Number(currentLine) + 1] + strArr[Number(currentLine) + 2];
+}
 
+function parseBofA(line){
+    console.log("Line:",line);
 
-        /* let newLines = 0;
-        for(const x in data.text) {
-            //console.log("Line: ",line);
-            if ( data.text[x] === "\n" ) {
-                //console.log("new line");
-                //const [data.text[x+1], data.text[x+2], data.text[x+3], data.text[x+4], data.text[x+5]] = data.text[x];
-                //console.log( data.text );
+    // first check to make sure its a debit
+    // the pattern below finds currency on the end
+    // prefixed with a minus sign
+    const purchase = line.match(/\-((\d{1,2}\,)?\d{1,3}\.\d{2})$/);
+    if ( purchase && purchase[0] !== "-0.00" ) {
+        console.log("purchase:",purchase[0]);
 
-                // const s = data.text[x+1];
-                // const m = s.match(/\d/);
-
-                // if ( m ) {
-                //     console.log("Match:",m);
-                // }
-
-
-                newLines++;
+        // withdrawls
+        //const withdrawl = line.match(/^(\d+\/\d+\/\d+)(Customer\s\w+(?:\s\w+))\-((\d{1,2}\,)?\d{1,3}\.\d{2})$/);
+        const withdrawl = line.match(/^(\d+\/\d+\/\d+)(.*(w|W)ithdrawal.*)\-((\d{1,2}\,)?\d{1,3}\.\d{2})$/);
+        if ( withdrawl ) {
+            let debt = {
+                date: withdrawl[1],
+                type: withdrawl[2],
+                amount: withdrawl[4]
             }
+            return debt;
         }
-        console.log("NewLines:",newLines); */
 
+        // checks
+        const check = line.match(/^(\d+\/\d+\/\d+)(\d{4})\-((\d{1,2}\,)?\d{1,3}\.\d{2})$/);
+        if ( check ) {
+            let debt = {
+                date: check[1],
+                type: check[2],
+                amount: check[3]
+            }
+            return debt;
+        }
 
-
-    } else if ( data.info.Author === "AmEx" ) {
-        console.log("AmEx");
+        // Debit, checkcard, payments
+        const match = line.match(/^(\d+\/\d+\/\d+)(.*)(\s\d{4}\s|\d+\/\d+\s|DES\:\w+|Bill)/);
+        let debt = {
+            date: match[1],
+            type: match[2],
+            amount: purchase[0]
+        }
+        //console.log(debt);
+        return debt;
     }
-
-    // // PDF metadata
-    //console.log(data.metadata); 
-    // // PDF.js version
-    // // check https://mozilla.github.io/pdf.js/getting_started/
-    //console.log(data.version);
-    // // PDF text
-    //console.log(data.text); 
-});
-
-
-let strLine = [];
-function atTo(str) {
-    if ( str !== "\n" ) atTo()
 }
 
 
@@ -107,40 +128,54 @@ function atTo(str) {
 
 
 
-/*
 
-^\$
-(\d{1,3}   (\,\d{3})*|(\d+)  )  (\.\d{2})   ?$
+/*
+(\s\d{4}\s|\d+\/\d+\s|DES\:\w+)
+
 
 
 // captures the date
 ((\d+\/){2}\d+)
 
+^(\d+\/\d+\/\d+)
+
+
+// captures the purchase date
+((\s\d{4}\s)|(\d+\/\d+\s))
+
+(\d{4}|\d+\/\d+)
+
+((?:\s\d{4}\s)|(?:\d+\/\d+\s))
+
+
 
 // captures the money
 (\d+\,)?(\d*)(\.\d{2})
 
+// captures curr at the end of the line
+(\d+\,)?(\d*)(\.\d{2})$
 
+// my curr capture 
+(\-(\d{1,2}\,)?\d{1,3}\.\d{2})$
+
+
+\-((\d{1,2}\,)?\d{1,3}\.\d{2})$
+
+
+
+
+
+^(\d+\/\d+\/\d+)(.*)(\s\d{4}\s|\d+\/\d+\s|DES\:\w+|Bill)
+
+
+// gets the start
+^(\d+\/\d+\/\d+)
+
+// captures the type
+(.*)
+
+// captures the purchase date
+(\s\d{4}\s|\d+\/\d+\s|DES\:\w+|Bill)
 
 
 */
-
-
-
-
-
-
-// const PDF = require('./');
-// const fs = require('fs');
-
-// let PDF_FILE = './test/statement.pdf';
-// let dataBuffer = fs.readFileSync('./test/statement.pdf');
-// PDF(dataBuffer).then(function(data) {
-//     fs.writeFileSync(`${PDF_FILE}.txt`, data.text, {
-//         encoding: 'utf8',
-//         flag: 'w'
-//     });
-//     debugger;
-// }).catch(function(err) {
-//     debugger;
-// });

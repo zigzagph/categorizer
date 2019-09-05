fe47550a8d8857c0d88b75da22ed746eea185df9
zigzagph <9909340+zigzagph@users.noreply.github.com>
2019-09-05 01:47:41 -0500

    disabled toast and added the ItemDialog

--- src/ShowData.js
+++ src/ShowData.js
@@ -3,8 +3,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import { Grid } from '@material-ui/core';
 import DataTable from './DataTable';
 import DataHeader from './DataHeader';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+// import { ToastContainer, toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+import ItemDialog from './ItemDialog';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -14,14 +15,22 @@ const useStyles = makeStyles(theme => ({
 
 export default ({docObj}) => {
     const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
     
-    toast.success('Successfully parsed...')
+    //toast.success('Successfully parsed...')
+
+    // open/close the dialog
+    const setDialog = (debt) => {
+        console.log("setDialog:", debt);
+        setOpen(!open);
+    }
 
     return (
         <Grid container className={classes.root}>
-            <ToastContainer />
+            {/* <ToastContainer /> */}
+            <ItemDialog open={open} close={setDialog}/>
             <DataHeader docObj={docObj}/>
-            <DataTable debts={docObj.debts} />
+            <DataTable debts={docObj.debts} close={(obj) => setDialog(obj)} />
         </Grid>
     );
 }

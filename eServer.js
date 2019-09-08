0b7c04697abf1edce472f0ad43c278f00d94def5
zigzagph <9909340+zigzagph@users.noreply.github.com>
2019-09-08 18:05:13 -0500

    Updated to handle multiple files

--- eServer.js
+++ eServer.js
@@ -47,13 +47,162 @@ app.post('/upload',function(req, res) {
           // An unknown error occurred when uploading.
         } 
 
+        // create an array of promises
+        const buffers = req.files.map(f => {
+            let dataBuffer = fs.readFileSync(f.destination + f.filename);
+            return pdf(dataBuffer);
+        })
+
+        // run all of the promises
+        Promise.all(buffers).then(data => {
+            // delete the uploaded files
+            req.files.forEach(f => fs.unlinkSync(f.destination + f.filename));
+            
+            // array to place the data after being parsed
+            let arrayOfFiles = [];
+
+            // loop to parse the data
+            for(const file of data) {
+                let arrayOfLines = [];
+                let pdfObj = {};
+
+                // set some of the return obj properties
+                pdfObj.author = file.info.Author;
+                pdfObj.pages = file.numpages;
+                pdfObj.skipped = 0;
+
+                if ( pdfObj.author === "CapitalOne" ) {
+                    console.log("Capital One Detected");
+                } else if ( pdfObj.author === "Bank of America" ) {
+                    console.log("Bank of America");
+        
+                    // split the large string into individual lines
+                    const strArr = file.text.split("\n");
+                    pdfObj.total = strArr.length;
+
+                    // loop over each line and construct complete lines
+                    // from the string array
+                    for(const x in strArr){
+
+                        // skip the interest earned lines. dont need that data
+                        if ( strArr[x].match(/^(?:\d+\/\d+\/\d+)(Interest\sEarned)(.*)/g) ) {
+                            pdfObj.skipped += 1;
+                            continue;
+                        }
+
+                        // extract the statements month range
+                        const encompass = strArr[x].match(/^for\s(\w+\s\d{2}\,\s\d{4})\sto\s(\w+\s\d{2}\,\s\d{4})$/);
+                        if ( encompass ) {
+                            //console.log("Encompass: ", encompass);
+                            pdfObj.startMonth = encompass[1];
+                            pdfObj.endMonth = encompass[2];
+                        }
+
+                        // ^ date match
+                        const dateMatch = strArr[x].match(/^(\d{2}\/\d{2}\/\d{2})(.*)/ig);
+                        // if there is a date at the beginning of the line then we can 
+                        // conclude that this is a debt or dep line
+                        if ( dateMatch ) {
+
+                            // $ currency match
+                            const mCur = dateMatch[0].match(/(\d+\,)?(\d*)(\.\d{2})$/ig);
+                            
+                            // next we check to see if the end of the line has a currency,
+                            // if there is no currency then the line is not complete and will
+                            // have to look ahead to complete the line
+
+                            // if the current line is not complete
+                            if ( !mCur ) {
+                                // if the current line does not end with a currency match then
+                                // I need to look ahead for the rest of the line data hense the
+                                // finishLine method to complete the line
+                                const line = finishLine(strArr, x);
+                                
+                                // push the complete line into the array
+                                arrayOfLines.push(line);
+                            } else {
+                                // the line is complete push the line into the array
+                                arrayOfLines.push(dateMatch[0]);
+                            }
+                        }
+                    }
+
+                    // the total number of complete lines
+                    pdfObj.complete = arrayOfLines.length;
+
+                    // construct the return obj
+                    let rtnObj = {
+                        ...pdfObj,
+                        ...parseBofA(arrayOfLines)
+                    }
+
+                    // delete the uploaded file
+                    //fs.unlinkSync(file.destination + file.filename);
+
+                    // write out the file
+                    // fs.writeFile('./example.txt', JSON.stringify(rtnObj), (err) => {
+                    //     if (err) {
+                    //         console.log(err);
+                    //         return;
+                    //     }
+                    //     console.log("File created");
+                    // })
+
+                    // next then
+                    //return rtnObj;
+                    arrayOfFiles.push(rtnObj);
+
+                } else if ( pdfObj.author === "AmEx" ) {
+                    console.log("AmEx");
+                }
+            }
+
+            // return the array of parsed data
+            return arrayOfFiles;
+
+        }).then( array => {
+            //console.log("Array:", array);
+
+            // return the data
+            //res.send( array );
+
+            // reduces and restructures the data array
+            // to make it easier on the front end
+            const rtn = array.reduce((a, c, i, ar) => {
+                !a ? c : a = {
+                    author: c.author,
+                    checks: Number(a.checks + c.checks),
+                    complete: Number(a.complete + c.complete),
+                    debits:  Number(a.debits + c.debits),
+                    debts: [...a.debts, ...c.debts],
+                    deposits: Number(a.deposits + c.deposits),
+                    endMonth: c.endMonth,
+                    pages: Number(a.pages + c.pages),
+                    skipped: Number(a.skipped + c.skipped),
+                    startMonth: ar[0].startMonth,
+                    total: Number(a.total + c.total),
+                    withdrawls: Number(a.withdrawls + c.withdrawls)
+                }
+                return ({...a});
+            })
+
+            //console.log("RTN:",rtn);
+            res.send( rtn );
+
+        }).catch( error => {
+            console.log("Error: ", error);
+            // return the error
+            res.send( error );
+        });
+
+        // single file iteration
         // iterate over each pdf file
-        for (const file of req.files) {
+        /* for (const file of req.files) {
             // buffer the contents of the pdf file
             let dataBuffer = fs.readFileSync(file.destination + file.filename);
 
             // parse the pdf data
-            pdf(dataBuffer).then(data => {
+            /* pdf(dataBuffer).then(data => {
 
                 let arrayOfLines = [];
                 let pdfObj = {};
@@ -132,13 +281,13 @@ app.post('/upload',function(req, res) {
                     fs.unlinkSync(file.destination + file.filename);
 
                     // write out the file
-                    fs.writeFile('./example.txt', JSON.stringify(rtnObj), (err) => {
-                        if (err) {
-                            console.log(err);
-                            return;
-                        }
-                        console.log("File created");
-                    })
+                    // fs.writeFile('./example.txt', JSON.stringify(rtnObj), (err) => {
+                    //     if (err) {
+                    //         console.log(err);
+                    //         return;
+                    //     }
+                    //     console.log("File created");
+                    // })
 
                     // next then
                     return rtnObj;
@@ -157,8 +306,8 @@ app.post('/upload',function(req, res) {
                 console.log("Error: ", error);
                 // return the error
                 res.send( error );
-            });            
-        }
+            });
+        } */
     })
 });
 
@@ -250,7 +399,7 @@ function parseBofA(lineArray){
     }
 
     // calculate the number of deposit lines
-    parsedObj.deposits = lineArray.length - parsedObj.debts.length;
+    parsedObj.deposits = lineArray.length - (parsedObj.debts.length - 1);
 
     return parsedObj;
 }
